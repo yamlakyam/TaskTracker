3c33ab15c567cd550f8aee112a41adfc5cfcc0c1
yamlakyam <yamlakyam@gmail.com>
2022-01-06 04:54:57 -0800

    onclick for the x buttons implemented

--- src/App.js
+++ src/App.js
@@ -44,13 +44,17 @@ const App = () => {
       day: "Feb 5th at 2:30pm",
       reminder: true,
     },
-  ])
+  ]);
+
+  const deleteTask = (id) => {
+    console.log('delete',id)
+  };
 
   return (
     <div className="container">
 
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 };
