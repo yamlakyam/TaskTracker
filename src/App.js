82499162acd31901e3f78cfa6bccbf653fd78ec2
yamlakyam <yamlakyam@gmail.com>
2022-01-06 03:49:16 -0800

    made the lists inisde a new prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
+
 
 // function App() {
 //   return (
@@ -22,11 +24,33 @@ class App extends React.Component{
 */
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+  ])
+
   return (
     <div className="container">
-      {/* <Header title={1}/> */}
+
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 };
--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,7 @@ const Button = ({color, text, onClick}) => {
 }
 
 Button.defaultProps={
-    color:'steelblue',
+    color:'blue',
     
 }
 
